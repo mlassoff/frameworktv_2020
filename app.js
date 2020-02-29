631dd5eca834b406593d76d00dc56187c931462e
gukanozadze <gukanozadze@gmail.com>
2020-03-01 00:12:26 +0400

    feat: add navigation

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const personImage = document.getElementById('person')
-
+const programs = document.getElementById('programs')
+const programsNavigation = document.getElementById('programs-navigation')
 
 const randomNumber = Math.ceil(Math.random() * 3)
-
 personImage.src = `./img/person-${randomNumber}.png`
+
+
+
+programs.addEventListener("mouseenter", event => {
+    programsNavigation.style.display = 'grid'
+})
+
+programsNavigation.addEventListener("mouseleave", event => {
+    programsNavigation.style.display = 'none'
+})
