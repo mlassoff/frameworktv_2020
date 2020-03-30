c4e29ca41233cb0ee445620ac99378685972c772
gukanozadze <gukanozadze@gmail.com>
2020-03-30 20:58:07 +0400

    refactor: sponsor images

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 // programs - Navigation in header
 const programs = document.getElementById('programs')
 const programsNavigation = document.getElementById('programs-navigation')
-
-const randomNumber = Math.ceil(Math.random() * 3)
-document.getElementById('person').src = `./img/person-${randomNumber}.png`
-
-
-
 programs.addEventListener("mouseenter", event => {
+    console.log('aeeeeeeee')
     programsNavigation.style.display = 'grid'
 })
 programsNavigation.addEventListener("mouseleave", event => {
     programsNavigation.style.display = 'none'
 })
+
+
+
+
+const person = document.getElementById('person')
+const randomNumber = Math.ceil(Math.random() * 3)
+person.src = `./img/person-${randomNumber}.png`
+
+
+
+
