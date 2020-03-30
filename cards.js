0639300272071cddd26cf0d74d62cd26edfd4904
Mark Lassoff <mark@frameworktv.om>
2020-03-30 19:57:00 -0400

    Added posters

--- cards.js
+++ cards.js
@@ -13,6 +13,8 @@
 let cat_coding = [];
 let cat_coding_projects = [];
 let cat_new_releases = [];
+const categories = [cat_new_releases, cat_codeing, cat_coding_projects];
+
 
 window.onload = function(){
     fetch('library.js')
@@ -26,28 +28,31 @@ window.onload = function(){
 }
 
 function parseVideos(videos){
-    for(x in videos){
-        let tags = videos[x].tags;
-        for(y in tags){
-            switch(tags[y]){
-                case "new": 
-                    cat_new_releases.push(videos[x]);
-                    break;
-                case "coding":
-                    cat_coding.push(videos[x]);
-                    break;
-                case "coding_projects":
-                    cat_coding_projects.push(videos[x]);
-                    break;
+    
+        for(x in videos){
+            let tags = videos[x].tags;
+            for(y in tags){
+                switch(tags[y]){
+                    case "new": 
+                        cat_new_releases.push(videos[x]);
+                        break;
+                    case "coding":
+                        cat_coding.push(videos[x]);
+                        break;
+                    case "coding_projects":
+                        cat_coding_projects.push(videos[x]);
+                        break;
+                }
             }
         }
-    }
     populatePage();
 }
 
-function populatePage(){
+function populatePage() {
     let out = "";
-    for(x in cat_new_releases){
+    console.log(cat_new_releases);
+    shuffle(cat_new_releases)
+    for(x=0; x < 4; x++){
         const imageURL = cat_new_releases[x].id + ".png";
         const title = cat_new_releases[x].title;
         out += "<div class='card'>";
@@ -55,6 +60,24 @@ function populatePage(){
         out += "<p>" + title + "</p>";
         out += "</div>";
     }
-    console.log(out);
     document.getElementById('new_releases').innerHTML = out;
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
