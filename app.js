36d7aba768e307d82d58bea8d46ed0ede8463e6b
gukanozadze <gukanozadze@gmail.com>
2020-03-30 20:13:41 +0400

    feat: category page

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const personImage = document.getElementById('person')
+// programs - Navigation in header
 const programs = document.getElementById('programs')
 const programsNavigation = document.getElementById('programs-navigation')
 
 const randomNumber = Math.ceil(Math.random() * 3)
-personImage.src = `./img/person-${randomNumber}.png`
+document.getElementById('person').src = `./img/person-${randomNumber}.png`
 
 
 
 programs.addEventListener("mouseenter", event => {
     programsNavigation.style.display = 'grid'
 })
-
 programsNavigation.addEventListener("mouseleave", event => {
     programsNavigation.style.display = 'none'
 })
